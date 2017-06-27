90ea018ba66744ba196d6f492af5587e15841615
Stefani Paulinelli <stefani@endlesslyorganic.com>
2017-06-26 20:15:33 -0400

    edit  portfolio raw stage

--- server.rb
+++ server.rb
@@ -3,3 +3,7 @@ require 'sinatra'
 get '/' do
   send_file File.expand_path('portfolio.html', settings.public_folder)
 end
+
+get '/about' do
+  send_file File.expand_path('about.html', settings.public_folder)
+end
